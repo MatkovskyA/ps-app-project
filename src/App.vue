00b94313320c771a202f5fea3660afa59ed32690
Матковский Анатолий <amatkovsky@runity.ru>
2025-10-09 09:49:24 +0300

    Старт проекта

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { computed } from 'vue';
+import Button from './components/button/Button.vue';
+const ident = 'fla';
+
+const colorClass = computed(() => {
+  return ident === 'flag' ? 'red' : 'blue';
+})
+
+</script>
+
+<template>
+  <header>Logo</header>
+  <main :id="ident" :class="colorClass">Content</main>
+  <div>
+
+  </div>
+  <footer>
+    <Button>
+      <template #icon>+</template>
+      Сохранить
+      <template #minus>-</template>
+      <template #[ident]>!!!</template>
+    </Button>
+  </footer>
+</template>
+
+<style scoped>
+.red {
+  color: red;
+}
+
+.blue {
+  color: blue;
+}
+</style>
---
+++ src/components/button/Button.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+<template>
+	<button class="button font-semibold">
+		<slot name="icon"></slot>
+		<slot></slot>
+		<slot name="minus"></slot>
+		<slot name="fla"></slot>
+	</button>
+</template>
+
+<style scoped>
+	.button {
+		border: none;
+		border-radius: 10px;
+		background: var(--gradient);
+		padding: 14px;
+		color: var(--color-primary);
+		cursor: pointer;
+	}
+
+	.button:hover {
+		background: var(--gradient-inverted);
+	}
+
+</style>
